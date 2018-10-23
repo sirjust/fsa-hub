3f098b0605771df0daa00974df1efea8200d4daa
Aspen James <aspenjames@tqca.org>
2018-10-22 17:32:04 -0700

    add and configure amplify

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
 import Routes from "./Routes";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import RouteNavItem from "./components/RouteNavItem";
 import './App.css';
+
+Amplify.configure(aws_exports);
 
 class App extends Component {
   constructor(props) {
