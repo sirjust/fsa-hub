54fd383b20255e9ed0f7739041c71bac1df90c44
Aspen James <aspenjames@tqca.org>
2018-11-06 19:30:58 -0800

    add outline of Tools component

--- react-app/src/containers/Tools.js
+++ react-app/src/containers/Tools.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
+const sanityClient = require('@sanity/client')
+const client = sanityClient({
+  projectId: 'proj_id', //idk where tf this is
+  dataset: 'dataset', //yea same ^
+  token: 'sanity-auth-token', // or leave blank to be anonymous user
+  useCdn: true // `false` if you want to ensure fresh data
+})
+
 export default class Tools extends Component {
 
   state = {
@@ -8,6 +16,13 @@ export default class Tools extends Component {
 
   componentDidMount() {
     //query here
+    const query = 'some sort of query';
+    const params = 'maybe we have params, tho idk'
+    client.fetch(query, params).then(tools => {
+      this.setState({
+        tools: [...tools]
+      });
+    });
   }
 
   render() {
@@ -26,5 +41,9 @@ export default class Tools extends Component {
     this.state.tools && this.state.tools.forEach(tool => (
       <li key={tool.name} onClick={this.handleClick} id={tool.id}>{tool.name}</li>
     ));
+  }
+
+  handleClick = event => {
+    this.props.history.push(`/tools/${event.target.id}`);
   }
 }
