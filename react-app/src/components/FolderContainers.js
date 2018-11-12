8111baee0e3d6ca8dff35e6771c76c39015bf9b7
thegreatsantini <lss13@my.fsu.edu>
2018-11-12 14:20:41 -0800

    reomved search component from the mid level directories and change to stateless. can be added later

--- react-app/src/components/FolderContainers.js
+++ react-app/src/components/FolderContainers.js
@@ -3,7 +3,7 @@ import DirectoryCard from "../components/DirectoryCard"
 
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import SearchComponent from './SearchComponent';
+// import SearchComponent from './SearchComponent';
 
 function TabContainer(props) {
 
@@ -14,42 +14,13 @@ function TabContainer(props) {
     );
 }
 
-export default class FolderContainers extends Component {
-    constructor() {
-        super()
-        this.state = {
-            folders: [],
-            filteredFolders: []
-        }
-    }
-    async componentDidMount() {
-        const data = await fetch("https://zp7mbokg.api.sanity.io/v1/data/query/production?query=*[_type == 'movie']{ _id, title, releaseYear }")
-        data.json().then(info => this.setState({ folders: info.result }))
-    }
-
-    handleSearch =  event => {
-        const searchedFolders = this.state.folders.filter(item => {
-            const normalizedSearch = event.target.value.replace(/[^A-Za-z0-9]+/gi, '')
-            const regex = new RegExp(normalizedSearch, 'i');
-            return item.title.match(regex) || false
-        });
-        this.setState({
-            filteredFolders: searchedFolders
-        })
-    };
-    render() {
-        const {filteredFolders}  = this.state;
+export default ({ folders }) => {
         return (
             <TabContainer>
-                <SearchComponent handleSearch={this.handleSearch} />
                 <Grid container spacing={24}>
-                    <DirectoryCard folders={ 
-                        filteredFolders.length < 1 
-                            ?this.state.folders
-                            :this.state.filteredFolders
-                } />
+                    <DirectoryCard folders={folders} />
                 </Grid>
             </TabContainer>
         );
     }
-}
+
