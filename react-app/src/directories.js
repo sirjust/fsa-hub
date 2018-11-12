9702b1bd1238c12e30d26a5868d5b048261fbb70
thegreatsantini <lss13@my.fsu.edu>
2018-11-12 14:18:53 -0800

    added a directories lib for sanity content

---
+++ react-app/src/directories.js
@@ -0,0 +1,38 @@
+export const fullStackApprenticeship = [
+    {
+        type: 'nativeSchema',
+        name: 'React Native'
+    },
+    {
+        type: 'webSchema',
+        name: 'React Web'
+    }, 
+    {
+        type: 'backEndSchema',
+        name: 'Node.js'
+    }, 
+    {
+        type: 'algorithmsSchema',
+        name: 'Applied Computer Science'
+    }, 
+    {
+        type: 'commandLineSchema',
+        name: 'Command Line Interface'
+    }, 
+    {
+        type: 'javascriptSchema',
+        name: 'JavaScript  - ES6 & Beyond'
+    }, 
+    {
+        type: 'gitSchema',
+        name: 'Git'
+    },
+    {
+        type: 'awsSchema',
+        name: 'Amazon Web Services'
+    },
+    {
+        type: 'securitySchema',
+        name: 'Security'
+    },
+]
