5f48e12fc090fcd909de28f6810942899906a005
thegreatsantini <lss13@my.fsu.edu>
2018-11-12 14:24:18 -0800

    getting info from directories lib. right now only working for one tab. but will add functionality to others next

--- react-app/src/containers/LinkTabs.js
+++ react-app/src/containers/LinkTabs.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import sanity from '../lib/sanity'
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import FolderContainers from "../components/FolderContainers";
-
+import { fullStackApprenticeship } from '../directories'
 
 function TabContainer(props) {
     
@@ -17,8 +17,6 @@ function TabContainer(props) {
     );
 }
 
-
-
 const styles = theme => ({
     root: {
         flexGrow: 1,
@@ -26,16 +24,28 @@ const styles = theme => ({
     },
 });
 
+const query = `*[_type == 'algorithmsSchema' || _type == 'gitSchema' || _type == 'nativeSchema' || _type == 'webSchema' || _type == 'backEndSchema' || _type == 'commandLineSchema' || _type == 'javascriptSchema' || _type == 'gitSchema' || _type == 'awsSchema' || _type == 'securitySchema'  ]{_type, text, title, url}`
+
 class LinkTabs extends Component {
     state = {
         value: 0,
+        fsaFolders: [],
+        cityFolders: [],
+        workFolders: []
     };
 
     handleChange = (event, value) => {
         this.setState({ value });
     };
 
+    async componentDidMount() {
 
+    const fsaFolders = fullStackApprenticeship.reduce((acc, next) => acc.concat(next.name), [])
+    // console.log(fsaFolders)
+    this.setState({ fsaFolders })
+    const test = await sanity.fetch(query);
+    console.log('test',test)
+}
 
     render() {
         const { classes } = this.props;
@@ -50,7 +60,7 @@ class LinkTabs extends Component {
                         <Tab label="Finding Work & Opportunity" />
                     </Tabs>
                 </AppBar>
-                {value === 0 && <FolderContainers />}
+                {value === 0 && <FolderContainers folders={fullStackApprenticeship.reduce((acc, next) => acc.concat({ name :next.name, type:next.type}), [])}/>}
                 {value === 1 && <TabContainer>Item Two</TabContainer>}
                 {value === 2 && <TabContainer>Item Three</TabContainer>}
             </div>
