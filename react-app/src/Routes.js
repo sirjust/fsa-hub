8201f897ab0ea6a0d436dc4492be99a0ee4643c5
thegreatsantini <lss13@my.fsu.edu>
2018-11-11 21:29:54 -0800

    add linkcomponent route

--- react-app/src/Routes.js
+++ react-app/src/Routes.js
@@ -5,10 +5,14 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Signup from './containers/Signup';
 import Login from './containers/Login';
+import LinkTabs from './containers/LinkTabs';
+import LinkComponent from './components/LinkComponent';
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path='/signup' exact component={Signup} props={childProps} />
     <AppliedRoute path='/login' exact component={Login} props={childProps} />
+    <AppliedRoute path='/knowledge' exact component={LinkTabs} props={childProps} />
+    <AppliedRoute path='/link/' component={LinkComponent} props={childProps} />
   </Switch>
