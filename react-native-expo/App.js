ca8238a88b248c380158ee07083ae0a3862252cb
David Fry <david.fry.tv@gmail.com>
2018-12-03 23:11:13 -0800

    GOAL: Setup up basic navigation for the app:Finished setting up the basic navigation for the app.

--- react-native-expo/App.js
+++ react-native-expo/App.js
@@ -1,21 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import {
+    createSwitchNavigator,
+    createStackNavigator,
+    createAppContainer,
+    createDrawerNavigator,
+    createBottomTabNavigator
+} from 'react-navigation'
+
+
+import AuthLoadingScreen from './screens/AuthLoadingScreen'
+import WelcomeScreen from "./screens/WelcomeScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import KnowledgeScreen from "./screens/KnowledgeScreen";
+import SettingsScreen from "./screens/SettingsScreen"
+
+
+const AuthStackNavigator = createStackNavigator({
+    Welcome: {screen: WelcomeScreen},
+    SignIn: {screen: SignInScreen},
+    SignUp: {screen: SignUpScreen}
+})
+
+const AppTabNavigator = createBottomTabNavigator({
+    Home: {screen: HomeScreen},
+    Chat: {screen: ChatScreen},
+    Knowledge: {screen: KnowledgeScreen}
+})
+
+const AppStackNavigator = createStackNavigator({
+    AppTabNavigator: {
+        screen: AppTabNavigator,
+        navigationOptions: ({navigation}) => ({
+            title: 'FSA Hub',
+            headerLeft: (
+                <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                    <View style={{ paddingHorizontal: 10}}>
+                        <Ionicons name="md-menu" size={32}/>
+                    </View>
+                </TouchableOpacity>
+            )
+        }),
+    }
+
+})
+
+const AppDrawerNavigator = createDrawerNavigator({
+    Home: AppStackNavigator,
+    Settings: {screen: SettingsScreen}
+})
+
+const AppNavigator = createSwitchNavigator ({
+    AuthLoading: AuthLoadingScreen,
+    Auth: AuthStackNavigator,
+    App: AppDrawerNavigator,
+
+})
+
+
+
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default createAppContainer(AppNavigator)
+
---
+++ react-native-expo/screens/SignUpScreen.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { View, StyleSheet, Text} from 'react-native';
+
+class SignUpScreen extends Component
+{
+    render() 
+    {
+        return (
+          <View style={styles.container}>
+            <Text>Sign Up Screen</Text>
+            
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+
+export default SignUpScreen;
