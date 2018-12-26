6e05c330e5b5e91035951dea7265c7146ceb57b0
Mikha'el <director@mikhaelbd.com>
2018-12-26 12:28:45 -0800

    Hooked up Amplify to app

--- react-native-expo/App.js
+++ react-native-expo/App.js
@@ -1,12 +1,50 @@
 import React, {Component} from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import Amplify, { Auth, API } from "aws-amplify";
+import awsmobile from "./aws-exports"
+import config from "./config"
 
 import AppNavigator from './config/navigation'
 
-class App extends Component
-{
-    render()
-    {
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+    async componentDidMount() {
+        this.configure()
+        console.log('Export info: ', awsmobile)
+
+    }
+
+    async configure() {
+        const result = await Amplify.configure({
+          Auth: {
+              mandatorySignIn: false,
+              region: config.cognito.REGION,
+              userPoolId: config.cognito.USER_POOL_ID,
+              identityPoolId: config.cognito.IDENTITY_POOL_ID,
+              userPoolWebClientId: config.cognito.APP_CLIENT_ID
+          },
+        //   Storage: {
+        //       region: config.s3.REGION,
+        //       bucket: config.s3.BUCKET,
+        //       identityPoolId: config.cognito.IDENTITY_POOL_ID
+        //   },
+        //   API: {
+        //       endpoints: [
+        //           {
+        //               name: "fsa",
+        //               endpoint: config.apiGateway.URL,
+        //               region: config.apiGateway.REGION
+        //           },
+        //       ]
+        //   }
+      });
+      console.log('Result: ', result);
+      }
+
+    render() {
         return (
           <AppNavigator/>
         );
