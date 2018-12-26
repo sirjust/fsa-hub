d337bb41e84910651b2773f1bf3edb61ebfd09ee
Mikha'el <director@mikhaelbd.com>
2018-12-26 12:26:44 -0800

    Auth working and styled

--- react-native-expo/screens/SignInScreen.js
+++ react-native-expo/screens/SignInScreen.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Button, AsyncStorage } from 'react-native';
+import { View, StyleSheet, Text, TextInput, AsyncStorage } from 'react-native';
+import { Auth } from "aws-amplify";
+import { Container, Header, Content, Form, Item, Input, Label, Button } from 'native-base';
 
 class SignInScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    }
+  }
   signIn = async () => {
+    await Auth.signIn(this.state.username, this.state.password);
+
     await AsyncStorage.setItem('userToken', 'fsa');
 
     this.props.navigation.navigate('App');
@@ -10,10 +22,33 @@ class SignInScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Sign In Screen</Text>
-        <Button title={'Complete Sign In'} onPress={() => this.signIn()} />
-      </View>
+      <Container>
+        <Content>
+          <Form>
+            <Item fixedLabel>
+              <Label>Username</Label>
+              <Input
+              placeholder="mikhael@mailinator.com"
+              returnKeyType="search"
+              value={this.state.username}
+              onChangeText={(username) => this.setState({ username })} 
+              autoCapitalize="none"
+              />
+            </Item>
+            <Item fixedLabel last>
+              <Label>Password</Label>
+              <Input 
+              placeholder="P@ssw0rd!"
+              returnKeyType="search"
+              value={this.state.password}
+              onChangeText={(password) => this.setState({ password})}
+              autoCapitalize="none"
+              />
+            </Item>
+        <Button full style={{backgroundColor: "#6200EE"}} onPress={() => this.signIn()}><Text style={{color: "white"}}>Sign In</Text></Button>
+          </Form>
+        </Content>
+      </Container>
     );
   }
 }
--- react-native-expo/screens/SignUpScreen.js
+++ react-native-expo/screens/SignUpScreen.js
@@ -1,12 +1,115 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, AsyncStorage } from 'react-native';
+import { Auth } from "aws-amplify";
+import { Container, Header, Content, Form, Item, Input, Label, Button } from 'native-base';
+
 
 class SignUpScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      confirm: false,
+      code: "",
+      user: {}
+    }
+  }
+
+  register = async () => {
+    const attributes = {
+      birthdate: "01/28/1994",
+      name: "Michael",
+      email: this.state.email, 
+      family_name: "Litchev",
+    }
+
+    const user = await Auth.signUp({
+      password: "P@ssw0rd!",
+      username: this.state.email,
+      attributes: attributes,
+    });
+
+    // console.log('USER OBJECT: ', JSON.stringify(user))
+    // console.log('Username: ', user.username)
+
+    await AsyncStorage.setItem('userToken', 'fsa');
+    
+    this.setState({ confirm: true, user: user })
+  };
+
+  confirm = async () => {
+    await Auth.confirmSignUp(this.state.email, this.state.code);
+
+    await AsyncStorage.setItem('userToken', 'fsa');
+
+    this.props.navigation.navigate('App');
+  };
+
+
+  renderConfirmation() {
+    return(
+      <Container>
+         <Content>
+          <Form>
+            <Item fixedLabel>
+              <Label>code</Label>
+              <Input
+              placeholder="123456"
+              returnKeyType="search"
+              value={this.state.code}
+              onChangeText={(code) => this.setState({ code })} 
+              autoCapitalize="none"
+              />
+            </Item>
+        <Button block style={{backgroundColor: "#6200EE", color: "white"}} onPress={() => this.confirm()}><Text style={{color: "white"}}>Complete Registration</Text></Button>
+          </Form>
+        </Content>
+      </Container>
+    )
+  }
+
+  renderRegister() {
+    return(
+    <Container>
+    <Content>
+          <Form>
+            <Item fixedLabel>
+              <Label>Username</Label>
+              <Input
+              placeholder="mikhael@mailinator.com"
+              returnKeyType="search"
+              value={this.state.email}
+              onChangeText={(email) => this.setState({ email })} 
+              autoCapitalize="none"
+              />
+            </Item>
+            <Item fixedLabel last>
+              <Label>Password</Label>
+              <Input 
+              placeholder="P@ssw0rd!"
+              returnKeyType="search"
+              value={this.state.password}
+              onChangeText={(password) => this.setState({ password})}
+              autoCapitalize="none"
+              />
+            </Item>
+        <Button full style={{backgroundColor: "#6200EE", color: "white"}} onPress={() => this.register()}><Text style={{color: "white"}}>Register</Text></Button>
+          </Form>
+        </Content>
+    </Container>
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Sign Up Screen</Text>
-      </View>
+      <Container>
+        {this.state.confirm === false ?
+        this.renderRegister() : this.renderConfirmation()  
+      }
+      </Container> 
     );
   }
 }
