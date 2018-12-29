0457fff6bed6bd8dd84ba5cd49e6733b6bdbb3ed
Mikha'el <director@mikhaelbd.com>
2018-12-28 21:00:50 -0800

    Assorted changes

--- react-native-expo/directories.js
+++ react-native-expo/directories.js
@@ -2,82 +2,98 @@ export const fullStackApprenticeship = [
     {
         type: 'gettingStartedSchema',
         name: 'Get Started',
-        description: 'The Full-Stack Apprenticeship starts with creating an account on #thehub, staying up to date with FSA Events and tracking your learning progress. Your first steps are to walk through the Serverless-Stack Tutorial, & walk through our recommended NodeSchool modules.'
+        description: 'Being your journey here. Start as an Apprentice, grow into a Developer, achieve mastery as an Engineer.',
+        tools: ['']
     },
     {
         type: 'nativeSchema',
         name: 'React Native',
-        description: 'Build iOS & Android applications using only JavaScript, without losing any performance. Potentially re-use 75% of your codebase across the web and mobile platforms. Expo offers great tooling to bootstrap application development.'
+        description: 'Build iOS & Android applications using only JavaScript, without losing any performance.',
+        tools: ['Expo']
     },
     {
         type: 'webSchema',
         name: 'React Web',
-        description: 'The FSA Technical Standard supports React as our choice for designing user experiences on the web or mobile phones. React allows developers to write their HTML, CSS & JS in the same file, offers thousands of open-source React components & Node Package Manager libraries.'
+        description: 'It started with the web, but it spread to mobile phones - React is the most effective way of creating digital experiences in 2019.',
+        tools: ['Material UI']
     }, 
     {
         type: 'backEndSchema',
         name: 'Node',
-        description: 'Node is the Technical Standards choice for a powerful, flexible programming language to build out the business logic your application needs. Promises and Async/Await functions allow you to write simple, neat asynchronous code perfect for web, mobile or IoT.'
+        description: 'A powerful, flexible programming language to build out the business logic your application needs. Write simple asynchronous code perfect for web, mobile or IoT.',
+        tools: ['Koa', 'DynamoDB', 'PostgreSQL']
     }, 
     {
         type: 'algorithmsSchema',
         name: 'Computer Science',
-        description: 'To progress from a Developer to Engineer, one must have a firm grasp of computer science techniques to optimize performance, write algorithms to sort & search through data & choose the ideal data structure when building a new service. '
+        description: 'To optimize the performance of your applications at scale, a knowledge of algorithms, data structures & basic electrical engineering is crucial. ',
+        tools: ['']
     }, 
     {
         type: 'commandLineSchema',
         name: 'Command Line',
-        description: 'The terminal is used for starting/stopping development servers, using Amazon Web Services, the Serverless framework & basic file-system organization such as creating a new file or folder. VS Code has a built in terminal FSA recommends instead of the default Terminal.'
+        description: 'The command-line, or terminal, is your best friend. Love it, do not feat it - you control the computer, not the other way around.',
+        tools: ['AWS Amplify', 'Serverless', 'Git']
     }, 
     {
         type: 'javascriptSchema',
         name: 'JavaScript',
-        description: 'JavaScript is the foundation upon which the FSA rests - we are working to demystify JS, which has roughly 40 words that you really need to understand to attain fluency in the language. '
+        description: 'JavaScript lets you write your front-end and back-end code with one language, as well as being the native language of the internet. ',
+        tools: ['Lodash']
     }, 
     {
         type: 'gitSchema',
         name: 'Git',
-        description: 'Developers need a way to collaborate on a code-base, and manage the changes that come with it. Its crucial to understand proper etiquette, best practices for version control & how to write an effective pull-request to submit for approval.'
+        description: 'Developers need a way to collaborate on a code-base, and manage the changes that come with it. Understand best practices for version control & how to write an effective pull-request.',
+        tools: ['']
     },
     {
         type: 'awsSchema',
         name: 'AWS',
-        description: 'Our Technical Standard lays out Amazon Web Services as the default choice for provisioning infrastructure. Whether thats hosting web content through S3, deploying serverless functions, provisioining a container or database - AWS is made for builders, and has the least terrible documentation of the major cloud providers. Our starting point: AWS Amplify.'
+        description: 'Whether you need to host web content through S3, deploy serverless functions or provision a container or database - AWS is made for builders.',
+        tools: ['S3', 'Lambda', 'Amplify', 'AppSync', 'Cognito']
     },
     {
         type: 'securitySchema',
         name: 'Security',
-        description: 'Most developers do not pay attention to security at all - that is one of your competitive advantages. Common security considerations: encrypting, storing and rotating database/API credentials. Cross-Site Scripting (XSS) prevention, Cross-site Request Forgery (CSRF) & SQL Injection must be addressed by Developers, with Engineers understanding how security is impacted by a resources networking & IaM permissions. '
+        description: 'Very few developers pay attention to security - this is one of your competitive advantages. Learn how to prevent Cross-Site Scripting (XSS), Cross-Site Request Forgery (CSRF) & SQL Injection.',
+        tools: ['Hunter2']
     },
     {
         type: 'paymentsSchema',
         name: 'Payments',
-        description: 'When building an application, the topic of how to accept/send money comes up pretty quick. There are a lot of rules for accepting money, (Stripe is the easiest, safest option) and we will lay out some other things you need to keep an eye on.'
+        description: 'CREAM GET THE MONEY - DOLLA DOLLA BILLS YALL',
+        tools: ['Stripe', 'Beyonic']
     },
     {
         type: 'serverlessSchema',
         name: 'Serverless',
-        description: 'The FSA Technical Standard advocates a serverless-first approach to back-end development - great for firing large volume, asynchronous requests that work with data '
+        description: 'FSA recommends a serverless-first approach to back-end development - great for firing large volume, asynchronous requests that work with data ',
+        tools: ['']
     },
     {
         type: 'learningSchema',
         name: 'Meta-Learning',
-        description: 'More than anything else, a Full-Stack Apprenticeship will teach you the skill of meta-learning: learning to learn. Focus on learning the 20% of content & technique to achieve 80% of the results you are looking for. The last fifth will come through experience'
+        description: 'Focus on learning the 20% of content to achieve 80% of the results you are looking for. The last fifth comes through experience',
+        tools: ['']
     },
     {
         type: 'devopsSchema',
         name: 'App Deployment',
-        description: 'Once you build you app, you need to deploy it. More than that, you need to set yourself up for success by making sure your solution can scale as your user activity increases. You need to own the full process of designing, building & deploying.'
+        description: 'Once you build you app, you need to deploy it - and FSA developer owns the full process of designing, building & deploying.',
+        tools: ['Travis CI', 'AWS CodeDeploy']
     },
     {
         type: 'testingSchema',
         name: 'Testing',
-        description: 'Its important to have the most critical parts of your application tested - this includes unit/integration tests on critical application functionality, strong error handling & a continous integration environment set up to run through your test suite.'
+        description: 'Use automated testing for the 20% most valuable functionality in your application. Manually testing every piece of functionality gets old pretty quick.',
+        tools: ['Jest', 'Chai', 'Mocha', 'Enzyme', 'Sinon']
     },
     {
         type: 'uxSchema',
         name: 'UX Design',
-        description: 'A UX Designer is a separate profession from a Full-Stack Developer, but the dev must understand the principles that drive the UX Design process so that they can be active collaborators and share the same vocabulary. There is no excuse for bad design.'
+        description: 'There is no excuse for bad design.',
+        tools: ['Sketch']
     }
 ]
 
@@ -109,41 +125,49 @@ export const findingWork = [
     {
         type: 'communitySchema',
         name: 'Community',
-        description: 'A list of online communities to complement your experience with the FSA.'
+        description: 'A list of online communities to complement your experience with the FSA.',
+        tools: ['']
     },
     {
         type: 'fullTimeSchema',
         name: 'Full Time Work',
-        description: 'Tips, tricks, strategy on how to tackle the full-time job search.'
+        description: 'Tips, tricks, strategy on how to tackle the full-time job search.',
+        tools: ['']
     },
     {
         type: 'apprenticeshipSchema',
         name: 'Apprenticeships',
-        description: 'Curated list of technical apprenticeship positions you should keep an eye on.'
+        description: 'Curated list of technical apprenticeship positions you should keep an eye on.',
+        tools: ['']
     },
     {
         type: 'freeLanceSchema',
         name: 'Freelance',
-        description: 'Business development strategy, process recommendations, working habits & opportunities relating to finding freelance work.'
+        description: 'Business development strategy, process recommendations, working habits & opportunities relating to finding freelance work.',
+        tools: ['']
     },
     {
         type: 'jobBoardSchema',
         name: 'Job Boards',
-        description: 'Lets get you paid'
+        description: 'Lets get you paid',
+        tools: ['']
     },
     {
         type: 'startupSchema',
         name: 'Startup Resources',
-        description: 'Curated list of resources to help you understand what it takes to succeed as a start-up. Document templates, incubators, finding co-founders and hiring.'
+        description: 'Curated list of resources to help you understand what it takes to succeed as a start-up. Document templates, incubators, finding co-founders and hiring.',
+        tools: ['']
     },
     {
         type: 'mentorshipSchema',
         name: 'Mentorship',
-        description: 'Curated list of resources to help you find mentorship, dispense it & everything else in between.'
+        description: 'Curated list of resources to help you find mentorship, dispense it & everything else in between.',
+        tools: ['']
     },
     {
         type: 'nomadSchema',
         name: 'Digital Nomadism',
-        description: 'Interested in digital nomadism? Learn how to break free, and succeed after doing so. Leverage your USD/EUR to live cheaply abroad, learn a second (third, fourth) language, give back by introducing the FSA to a new country.'
+        description: 'Interested in digital nomadism? Learn how to break free, and succeed after doing so. Leverage your USD/EUR to live cheaply abroad, learn a second (third, fourth) language, give back by introducing the FSA to a new country.',
+        tools: ['']
     }
 ]
