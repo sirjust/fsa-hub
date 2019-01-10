53d2465b45ffb046705057c470714c8dda05fd47
rjgrunau <robertgrunau@gmail.com>
2019-01-10 09:52:46 -0800

    fixed typo in createResource that prevented the file from being loaded

--- backend/fsa-sls/createResource.js
+++ backend/fsa-sls/createResource.js
@@ -1,51 +1,57 @@
 import uuid from "uuid";
 import AWS from "aws-sdk"; 
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function main(main, context, callback) {
-    const data = JSON.parse.apply(event.body);
+export function main(event, context, callback) {
+    const data = JSON.parse(event.body);
 
     const params = {
         TableName: "resources", 
 
 
-        item: {
-            resourceId: event.requestContext.identity.congnitoIdentityId,
-            resoureHash: uuid.v1(),
+        Item: {
+            resourceId: event.requestContext.identity.cognitoIdentityId,
+            resourceHash: uuid.v1(),
             resourceName: data.resourceName,
             directory: data.directory,
             schemaType: data.schemaType,
             resourceUrl: data.resourceUrl,
             resourceAuthor: data.resourceAuthor,
             approved: data.approved,
-            createAt: Date.now()
+            submittedOn: Date.now()
         }
         
     };
-}
 
-dynamoDB.put(params, (error, data) => {
-    const headers = {
-        "Acess-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true
-    };
-
-    if (error) {
-        const response = {
+    dynamoDb.put(params, (error, data) => {
+        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        };
+    
+        // Return status code 500 on error
+        if (error) {
+          const response = {
             statusCode: 500,
             headers: headers,
-            body: JSON.stringify({ status: false})
+            body: JSON.stringify({ status: false })
+          };
+          callback(null, response);
+          return;
         }
-        callback( null, response);
-        return;
-    }
+    
+        // Return status code 200 and the newly created item
+        const response = {
+          statusCode: 200,
+          headers: headers,
+          body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+      });
+}
 
-    const response = {
-        statusCode: 200,
-        headers: headers,
-        body: JSON.stringify(params.Item)
-    };
-    callback( null, response );
-});
+
+
 
