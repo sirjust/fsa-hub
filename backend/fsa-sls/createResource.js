bda41e9437ec1a4296bc10625effe37682bf2895
rjgrunau <robertgrunau@gmail.com>
2019-01-10 09:19:14 -0800

    wrote serverless function to create resources and made mocks file to handle local tests

--- backend/fsa-sls/createResource.js
+++ backend/fsa-sls/createResource.js
@@ -3,7 +3,7 @@ import AWS from "aws-sdk";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-export function createResource(main, context, callback) {
+export function main(main, context, callback) {
     const data = JSON.parse.apply(event.body);
 
     const params = {
