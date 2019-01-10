75f082aa5d84ad5612904ec4e0f1eb7ad745a979
rjgrunau <robertgrunau@gmail.com>
2019-01-10 09:00:11 -0800

    wrote function for creating resourses in createResources.js

--- backend/fsa-sls/createResource.js
+++ backend/fsa-sls/createResource.js
@@ -5,5 +5,47 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 export function createResource(main, context, callback) {
     const data = JSON.parse.apply(event.body);
+
+    const params = {
+        TableName: "resources", 
+
+
+        item: {
+            resourceId: event.requestContext.identity.congnitoIdentityId,
+            resoureHash: uuid.v1(),
+            resourceName: data.resourceName,
+            directory: data.directory,
+            schemaType: data.schemaType,
+            resourceUrl: data.resourceUrl,
+            resourceAuthor: data.resourceAuthor,
+            approved: data.approved,
+            createAt: Date.now()
+        }
+        
+    };
 }
+
+dynamoDB.put(params, (error, data) => {
+    const headers = {
+        "Acess-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+    };
+
+    if (error) {
+        const response = {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ status: false})
+        }
+        callback( null, response);
+        return;
+    }
+
+    const response = {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(params.Item)
+    };
+    callback( null, response );
+});
 
