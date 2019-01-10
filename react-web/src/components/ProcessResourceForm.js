4cd78d1f31e0de32aca96bc839b357a8360b8948
Vegitarius <gordonv15@gmail.com>
2019-01-10 09:36:55 -0800

    added component for new resource and reviewing resource

---
+++ react-web/src/components/ProcessResourceForm.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import {
+  fullStackApprenticeship,
+  cityByCity,
+  findingWork
+  } from '../directories';
+import Button from '@material-ui/core/Button';
+
+
+export default class NewResourceForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      directory: props.formInfo.directory,
+      schemas: [],
+      schema: this.props.formInfo.schema
+    };
+  }
+
+  async componentDidMount() {
+    await this.updateSchemas();
+  }
+
+  updateSchemas() {
+    this.setState({ schema: '' })
+    switch (this.state.directory) {
+      case 'FSA':
+        this.setState({ schemas: fullStackApprenticeship})
+        break;
+      case 'City Guide':
+        this.setState({ schemas: cityByCity})
+        break;
+      case 'Getting Paid':
+        this.setState({ schemas: findingWork})
+        break;
+      default:
+        break;
+    }
+  }
+
+  changeDirectory = async e => {
+    await this.setState({
+      directory: e.target.value
+    })
+    await this.updateSchemas();
+  }
+
+  render() {
+    let { formInfo } = this.props
+    let schemas = this.state.schemas.map((item, i) => 
+      <option key={i} value={item.name}>{item.name}</option>)
+    return (
+      <form className='newResourceForm'>
+        <div className="formElement">
+          <label>Resource Name</label>
+          <br />
+          <input
+              type="text"
+              id="resourceName"
+              value={formInfo.name}
+              required
+          />
+        </div>
+        <br />
+
+        <div className="formElement">
+          <label>Directory</label>
+          <br />
+          <select value={this.state.directory || formInfo.directory} onChange={(e) => this.changeDirectory(e)} required>
+            <option value='FSA'>FSA</option>
+            <option value='City Guide'>City Guide</option>
+            <option value='Finding Work'>Getting Paid</option>
+          </select>
+          <br />
+
+        </div>
+        <br />
+
+        <div className='formElement'>
+          <label>Schema Type</label>
+          <br />
+          <select onChange={this.updateSchemas} required>
+            {this.state.schema ? <option>{this.state.schema}</option> : schemas }
+          </select>
+        </div>
+        <br />
+        
+        <div className="formElement">
+          <label>Description</label>
+          <br />
+          <input
+              type="text"
+              id="resourceDescription"
+              value={formInfo.description}
+              required
+          />
+        </div>
+        <br />
+        
+        <div className="formElement">
+          <label>URL</label>
+          <br />
+          <input
+              type="url"
+              id="resourceUrl"
+              value={formInfo.url}
+              required
+          />
+          <br />
+          <Button href={formInfo.url}>Visit Url</Button>
+        </div>
+        <br />
+
+        <div className="formElement">
+          <label>Resource Author</label>
+          <br />
+          <input
+              type="text"
+              id="resourceAuthor"
+              value={formInfo.author}
+              required
+          />
+        </div>
+        <br />
+
+        <div className="formElement">
+          <label>Rank</label>
+          <br />
+          <input
+              type="number"
+              id="resourceRank"
+              min="1"
+              max="100"
+              placeholder={formInfo.rank}
+              required
+          />
+          <p className='small'>
+            Give the content a rank between 1 and 100
+          </p>
+        </div>
+        <br />
+
+        {/* Button need to either approve request and add to schemas 
+        in proper directory, or delete request */}
+        <Button>
+          Approve Resource
+        </Button>
+        <Button>
+          Deny Resource
+        </Button>
+
+      </form>
+    )
+  }}
