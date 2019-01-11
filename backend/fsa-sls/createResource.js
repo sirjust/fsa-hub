7c505f8d0810915c3a0f468c3845b13ad8984542
rjgrunau <robertgrunau@gmail.com>
2019-01-10 20:08:08 -0800

    refactored dynamoDb function in to seperate file, and imported into createResponse.js

--- backend/fsa-sls/createResource.js
+++ backend/fsa-sls/createResource.js
@@ -1,7 +1,9 @@
 import uuid from "uuid";
-import AWS from "aws-sdk"; 
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+
 
 export function main(event, context, callback) {
     const data = JSON.parse(event.body);
@@ -24,32 +26,14 @@ export function main(event, context, callback) {
         
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true
-        };
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (error) {
+        console.log(error);
+        return failure({ status: false });
+    }
     
-        // Return status code 500 on error
-        if (error) {
-          const response = {
-            statusCode: 500,
-            headers: headers,
-            body: JSON.stringify({ status: false })
-          };
-          callback(null, response);
-          return;
-        }
-    
-        // Return status code 200 and the newly created item
-        const response = {
-          statusCode: 200,
-          headers: headers,
-          body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-      });
 }
 
 
