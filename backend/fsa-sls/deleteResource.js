cb62bffd44897573e383cc12fb2bc2932a410e84
rjgrunau <robertgrunau@gmail.com>
2019-01-11 22:42:29 -0800

    wrote function to delete a resource

---
+++ backend/fsa-sls/deleteResource.js
@@ -0,0 +1,23 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import {success, failure } from './libs/response-lib'; 
+
+
+export async function main (event, context) {
+    const params = {
+        TableName: "resources", 
+
+        Key: {
+            resourceId: event.requestContext.identity.cognitoIdentityId,
+            resourceHash: event.pathParameters.id
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("delete", params);
+        
+        return success({status: true})
+    } catch (error) {
+        console.log(error)
+        return failure ({ status: false})
+    }
+}
