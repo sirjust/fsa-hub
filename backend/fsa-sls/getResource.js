e5d41f2af202fef33a58143f1ecef9c857f4ead1
rjgrunau <robertgrunau@gmail.com>
2019-01-11 20:38:00 -0800

    wrote function to get a resource and configured the endpoint

---
+++ backend/fsa-sls/getResource.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib'; 
+
+
+export async function main (event, context) {
+    const params = {
+        TableName: 'resources', 
+
+        Key: {
+            resourceId: event.requestContext.identity.cognitoIdentityId,
+            resourceHash: event.pathParmeters.id
+        }
+    }; 
+
+    try {
+        const result = await dynamoDbLib.call(result.Item);
+
+        if (result.Item){
+            return success(result.Item)
+        }else
+            return failure({status: false, error: "item not found."})
+    } catch (error) {
+        console.log(error)
+        return failure ({ status: false})
+    }
+}
