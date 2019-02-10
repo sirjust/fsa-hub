c52ab85a5e2d2bc6c1d08b99fbc3522191c0009c
Mikha'el <director@mikhaelbd.com>
2019-02-09 21:55:27 -0800

    Updated calendar event to improve card item

--- react-native-expo/components/Event.js
+++ react-native-expo/components/Event.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { Button, Text, ListItem, Left, Right, Body, Thumbnail } from 'native-base';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text
+} from 'react-native';
+import {
+  RkButton,
+  RkText,
+  RkCard,
+} from 'react-native-ui-kitten';
+import { UtilStyles } from '../style/styles';
 import moment from "moment";
 
-export default class Event extends React.Component {
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
   render() {
+    const { navigation } = this.props;
     return (
-        <ListItem>
-              {/* <Left>
-                <Thumbnail source={this.props.avatar} />
-              </Left> */}
-              <Body>
-                <Text style={{ color: 'white', fontSize: 18, fontStyle: "italic"}}>{this.props.name}</Text>
-                <Text note style={{ color: 'white'}}>{this.props.description}</Text>
-                <Text note style={{ color: 'white', paddingTop: 10}}>{moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a').split(",")[0]} | {this.props.start} - {this.props.end}</Text>
-                
-              </Body>
-            </ListItem>
+      <View style={{ flex: 1, paddingBottom: 15 }}>
+        <ScrollView
+          // automaticallyAdjustContentInsets={true}
+          // style={[UtilStyles.container, styles.screen]}
+          >
+          <RkCard>
+            <View rkCardHeader={true}>
+              <View>
+                <RkText rkType='header'>Upcoming: {this.props.name}</RkText>
+                <RkText rkType='subtitle'>{moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a').split(",")[0]} | {this.props.start} - {this.props.end}</RkText>
+              </View>
+            </View>
+            <View rkCardContent={true} style={{ paddingTop: 0 }}>
+              <RkText rkType='compactCardText'>
+                {this.props.description}
+              </RkText>
+            </View>
+            {/* <View rkCardFooter={true}>
+              <View style={styles.footerButtons}>
+                <RkButton rkType='outline' style={{ marginRight: 16 }}>+ CALENDAR
+                </RkButton>
+                <RkButton rkType='outline'>EXPLORE</RkButton>
+              </View>
+            </View> */}
+          </RkCard>
+        </ScrollView>
+      </View>
     );
   }
 }
