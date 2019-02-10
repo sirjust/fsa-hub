030f11c5873dad45e2ac41430a5e2b8cd5f7aaee
Mikha'el <director@mikhaelbd.com>
2019-02-09 21:52:07 -0800

    Updated signout logic

--- react-native-expo/App.js
+++ react-native-expo/App.js
@@ -21,35 +21,9 @@ class App extends Component {
           });
     }
 
-//     async configure() {
-//         const result = await Amplify.configure({
-//           Auth: {
-//               mandatorySignIn: false,
-//               region: config.cognito.REGION,
-//               userPoolId: config.cognito.USER_POOL_ID,
-//               identityPoolId: config.cognito.IDENTITY_POOL_ID,
-//               userPoolWebClientId: config.cognito.APP_CLIENT_ID
-//           },
-//         //   Storage: {
-//         //       region: config.s3.REGION,
-//         //       bucket: config.s3.BUCKET,
-//         //       identityPoolId: config.cognito.IDENTITY_POOL_ID
-//         //   },
-//           API: {
-//               endpoints: [
-//                   {
-//                       name: "eventslambda",
-//                       endpoint: config.apiGateway.URL,
-//                       region: config.apiGateway.REGION
-//                   },
-//               ]
-//           }
-//       });
-//   }
-
     render() {
         return (
-          <AppNavigator/>
+          <AppNavigator screenProps={{...this.props}}/>
         );
     }
 }
